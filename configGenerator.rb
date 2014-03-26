562fd5e78fe93451ab084003e7bfed9c91d5a8aa
Jérémy Autran <jeremy.autran@gmail.com>
2014-03-26 14:07:50 +0100

    Le générateur

--- configGenerator.rb
+++ configGenerator.rb
@@ -1,5 +1,11 @@
 require 'set'
 
+
+# Ici, on va créer des paniers : un par zone
+# On met chaque capteur dans le/les paniers corresondant aux zones qu'ils
+# observent.
+# ensuite on fait un produit cartesien des paniers pour avoir toutes les
+# configurations.
 def methodeTotale(donnees)
     sols = []
     baskets = {}
@@ -22,6 +28,10 @@ def methodeTotale(donnees)
     return sols
 end
 
+# Ici, l'idée c'est de prendre le maximum de paire de capteurs qui voient
+# toutes les zones. Ca peut donner une bonne partie des solutions dans les
+# scénarios ou le nombre de zones / sensor est proche du nombre de zones total
+# O(n^2) 
 def methode1(donnees)
     sols = []
     zones = Set.new(donnees.getZones)
@@ -41,7 +51,58 @@ def methode1(donnees)
 
 end
 
+# On fait un passage sur tous les capteurs, on les ajoute à une liste et 
+# on passe à la liste suivante quand toutes les zones sont remplies
+# O(n)
+def methode2(donnees)
+    sols = []
+    zones = Set.new donnees.getZones
+    tzones = Set.new
+    tconf = []
+    donnees.getCapteurs.each do |x|
+        #on ajoute le capteur à la configuration
+        tconf.push x
+        #On ajoute les zones du capteur aux 
+        x.getZones.each do |z|
+            tzones.add z
+        end
+        if zones == tzones
+            sols.push tconf.clone
+            tzones.clear
+            tconf.clear
+        end
+    end
+    return sols
+end
+
+def methode2random(donnees)
+    sols = []
+    zones = Set.new donnees.getZones
+    tzones = Set.new
+    tconf = []
+    cl = donnees.getCapteurs.clone 
+    while cl.size > 0 do
+        #On tire au hazard, et supprime l'élément de la liste
+        x = cl.delete cl.sample
+        #on ajoute le capteur à la configuration
+        tconf.push x
+        #On ajoute les zones du capteur aux 
+        x.getZones.each do |z|
+            tzones.add z
+        end
+        if zones == tzones
+            sols.push tconf.clone
+            tzones.clear
+            tconf.clear
+        end
+    end
+    return sols
+
+end
+
 def generateConfigurations(donnees)
+    return methode2random donnees
+    return methode2 donnees
     return methodeTotale donnees
-    return methode1(donnees)
+    return methode1 donnees
 end
