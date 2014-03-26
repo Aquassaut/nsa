8d0820b13e22bc67a569b0cdc538318b6a2cb187
Jérémy Autran <jeremy.autran@gmail.com>
2014-03-26 15:17:26 +0100

    nouvelle methode

--- configGenerator.rb
+++ configGenerator.rb
@@ -75,6 +75,7 @@ def methode2(donnees)
     return sols
 end
 
+#La même avec un ordre de pick random
 def methode2random(donnees)
     sols = []
     zones = Set.new donnees.getZones
@@ -97,12 +98,70 @@ def methode2random(donnees)
         end
     end
     return sols
+end
+
+#Ici, l'idée c'est de découper les capteurs en deux
+#bucket : les capteurs qui ont beaucoup de zones et les
+#capteurs qui ont peu de zones. Ensuite, on parcoure
+#les capteurs a beaucoup de zones et on les complète avec
+#des capteurs à peu de zones
+def methode3(donnees)
+    sols = []
+    zones = Set.new donnees.getZones
+
+    sortedCapteurs = donnees.getCapteurs.sort {|x, y| x.getZones.size <=> y.getZones.size }
+    #on met le "pivot" au milieu, ça peut être amélioré
+    pivot = sortedCapteurs.size / 2
+    petitsCapteurs = sortedCapteurs.slice 0, pivot
+    grosCapteurs = sortedCapteurs.slice pivot, sortedCapteurs.size - 1
+
+    grosCapteurs.each do |x|
+        sol = [ x ]
+        tzones = Set.new x.getZones
+        petitsCapteurs.each do |y|
+            break if tzones == zones
+            unless tzones.superset? Set.new y.getZones
+                y.getZones.each { |v| tzones.add v }
+                sol.push petitsCapteurs.delete y
+            end
+        end
+        sols.push sol if tzones == zones
+    end
+    return sols
+end
+
+#La méthode 3 mais avec un pick random
+def methode3random(donnees)
+    sols = []
+    zones = Set.new donnees.getZones
+
+    sortedCapteurs = donnees.getCapteurs.sort {|x, y| x.getZones.size <=> y.getZones.size }
+    #on met le "pivot" au milieu, ça peut être amélioré
+    pivot = sortedCapteurs.size / 2
+    petitsCapteurs = sortedCapteurs.slice 0, pivot
+    grosCapteurs = sortedCapteurs.slice pivot, sortedCapteurs.size - 1
+
+    grosCapteurs.shuffle.each do |x|
+        sol = [ x ]
+        tzones = Set.new x.getZones
+        petitsCapteurs.shuffle.each do |y|
+            break if tzones == zones
+            unless tzones.superset? Set.new y.getZones
+                y.getZones.each { |v| tzones.add v }
+                sol.push petitsCapteurs.delete y
+            end
+        end
+        sols.push sol if tzones == zones
+    end
+    return sols
 
 end
 
 def generateConfigurations(donnees)
+    return methodeTotale donnees
+    return methode3random donnees
+    return methode3 donnees
     return methode2random donnees
     return methode2 donnees
-    return methodeTotale donnees
     return methode1 donnees
 end
